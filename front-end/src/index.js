270ac9c795106d9db7af97c58695df22b3bb31af
renatoTheoJr <renatotheojunior@gmail.com>
2023-06-09 16:30:59 -0300

    Página Home

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import Router from './pages/router';
+import Header from './components/header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    <Header />
     <Router />
   </React.StrictMode>
 );
